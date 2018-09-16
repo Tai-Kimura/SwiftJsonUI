ed760ec53becb151609a94ea22d692aa2173086d
木村太一朗 <kimura@tanosys.com>
2018-09-16 10:41:36 +0900

    fix binding bug

--- SwiftJsonUI/Classes/Binding.swift
+++ SwiftJsonUI/Classes/Binding.swift
@@ -17,15 +17,25 @@ open class Binding: NSObject {
     }
     
     public func bindView() {
-        let mirror = Mirror(reflecting: self)
-        let properties = mirror.children.filter{$0.label != nil}.map{$0.label}
+        let properties = getProperties()
         if let views = _viewHolder?._views {
             for v in views.values {
-                if let propertyName = v.propertyName, let index = properties.index(of: propertyName), let property = properties[index] {
+                if let propertyName = v.propertyName, let index = properties.index(of: propertyName) {
+                    let property = properties[index]
                     self.setValue(v, forKeyPath: property)
                 }
             }
         }
+    }
+    
+    private func getProperties() -> [String] {
+        var mirror = Mirror(reflecting: self)
+        var properties = mirror.children.filter{$0.label != nil}.map{$0.label}
+        while let sMirror = mirror.superclassMirror, sMirror.subjectType is Binding.Type {
+            properties.append(contentsOf: sMirror.children.filter{$0.label != nil}.map{$0.label})
+            mirror = sMirror
+        }
+        return properties as! [String]
     }
     
     override open func setValue(_ value: Any?, forUndefinedKey key: String) {
