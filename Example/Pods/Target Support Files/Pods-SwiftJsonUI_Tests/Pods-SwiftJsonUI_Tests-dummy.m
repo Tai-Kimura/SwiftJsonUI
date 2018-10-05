fd460a90f11fbd17a9b2c6df7c0c1be2540f6d7a
木村太一朗 <kimura@tanosys.com>
2018-10-05 16:54:33 +0900

    remove example

--- Example/Pods/Target Support Files/Pods-SwiftJsonUI_Tests/Pods-SwiftJsonUI_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SwiftJsonUI_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_SwiftJsonUI_Tests
-@end
--- Example/Pods/Target Support Files/SwiftJsonUI/SwiftJsonUI-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_SwiftJsonUI : NSObject
-@end
-@implementation PodsDummy_SwiftJsonUI
-@end
