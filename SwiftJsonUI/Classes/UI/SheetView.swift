37f9530058458ffb62e085f681b29094e87d130b
木村太一朗 <kimura@tanosys.com>
2020-10-09 18:07:01 +0900

    date picker bug fix

--- SwiftJsonUI/Classes/UI/SheetView.swift
+++ SwiftJsonUI/Classes/UI/SheetView.swift
@@ -3,42 +3,24 @@
 //  SwiftJsonUI
 //  Created by Taichiro Kimura on 2016/01/15.
 import UIKit
-
 @objc public protocol SheetViewDelegate {
-    
     @objc optional func didPickItem(row: Int, inComponent component: Int)
-    
     @objc optional func didPickDate(_ date: Date)
-    
     @objc optional func dismissWithPick(row: Int, forComponent component: Int)
-    
     @objc optional func backWithPick(row: Int, forComponent component: Int)
-    
     @objc optional func dismissWithPickDate(_ date: Date)
-    
     @objc optional func backWithPickDate(_ date: Date)
 }
-
 open class SheetView: NSObject, UIPickerViewDelegate, UIPickerViewDataSource {
-    
     public static var font: UIFont = UIFont.systemFont(ofSize: 20.0)
-    
     public static var textColor: UIColor = UIColor.black
-    
     public static var backgroundColor = UIColor.colorWithHexString("000000", alpha: 0.2)
-    
     public static var selectBtnColor = SJUIViewCreator.defaultFontColor
-    
     public static var backBtnColor = SJUIViewCreator.defaultFontColor
-    
     public static var lineColor = UIColor.lightGray
-    
     public static var selectBtnTitle = "選択"
-    
     public static var backBtnTitle = "前へ"
-    
     fileprivate static let instance = SheetView()
-    
     fileprivate var _view: UIView!
     public var _customView: UIView!
     fileprivate var _pickerView: UIPickerView!
@@ -46,15 +28,10 @@ open class SheetView: NSObject, UIPickerViewDelegate, UIPickerViewDataSource {
     fileprivate var _itemNames: [String] = Array<String>()
     fileprivate var _backBtn: UIButton!
     fileprivate var _selectBtn: UIButton!
-    
     public weak var delegate: SheetViewDelegate?
-    
     public var pickerSource: [[Any]] = Array<[Any]>()
-    
     fileprivate override init() {
-        
     }
-    
     public class func sharedInstance() -> SheetView {
         if (instance._view == nil) {
             instance._view = UIView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.size.width, height: UIScreen.main.bounds.size.height))
@@ -66,35 +43,35 @@ open class SheetView: NSObject, UIPickerViewDelegate, UIPickerViewDataSource {
             frame.origin.y = UIScreen.main.bounds.height + 100.0
             instance._customView.frame = frame
             instance._view.addSubview(instance._customView)
-            
         }
         return instance
     }
-    
-    
     public func createPickerView() -> UIView {
         let customView = UIView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.size.width, height: 300.0))
         customView.backgroundColor = UIColor.white
         _pickerView = UIPickerView(frame: CGRect(x: 0, y: 20.0, width: UIScreen.main.bounds.size.width, height: 280.0))
         _pickerView.delegate = self
         _pickerView.dataSource = self
-        
-        _datePicker = UIDatePicker(frame: CGRect(x: 0, y: 20.0, width: UIScreen.main.bounds.size.width, height: 280.0))
-        
+        _datePicker = UIDatePicker()
         _datePicker.calendar = getCalendar()
-        
         _datePicker.datePickerMode = UIDatePicker.Mode.date
-        
+        if #available(iOS 14.0, *) {
+            _datePicker.tintColor = SheetView.textColor
+            _datePicker.preferredDatePickerStyle = .wheels
+            _datePicker.frame = CGRect(x: 0, y: 20.0, width: UIScreen.main.bounds.width, height: 280.0)
+        } else {
+            _datePicker.setValue(SheetView.textColor, forKeyPath: "textColor")
+            if #available(iOS 13.0, *) {
+                _datePicker.setValue(false, forKey: "highlightsToday")
+            }
+        }
         _datePicker.addTarget(self, action: #selector(SheetView.dateChanged), for: UIControl.Event.valueChanged)
-        
         let lineView = UIView(frame: CGRect(x: 0, y: 40.0, width: customView.frame.size.width, height: 1.0))
         lineView.backgroundColor = SheetView.lineColor
-        
         _selectBtn = UIButton(frame: CGRect(x: customView.frame.size.width - 70.0, y: 0, width: 70.0, height: 40.0))
         _selectBtn.setTitleColor(SheetView.selectBtnColor, for: UIControl.State())
         _selectBtn.setTitle(SheetView.selectBtnTitle, for: UIControl.State())
         _selectBtn.addTarget(self, action: #selector(SheetView.selected), for: UIControl.Event.touchDown)
-        
         _backBtn = UIButton(frame: CGRect(x: 0, y: 0, width: 70.0, height: 40.0))
         _backBtn.setTitleColor(SheetView.backBtnColor, for: UIControl.State())
         _backBtn.setTitle(SheetView.backBtnTitle, for: UIControl.State())
@@ -110,11 +87,9 @@ open class SheetView: NSObject, UIPickerViewDelegate, UIPickerViewDataSource {
         customView.addSubview(lineView)
         return customView
     }
-    
     public func setSelectBtnTitle(_ title: String) {
         _selectBtn.setTitle(title, for: UIControl.State())
     }
-    
     public func showPicker(_ selectRows:[Int], withDataSource datasource:[[Any]], forItem itemNames: [String], inView mainView: UIView, canBack: Bool = false, duration: TimeInterval = 0.3, completion: ((Bool) -> Void)? = nil) {
         _pickerView.alpha = 1.0
         _datePicker.alpha = 0
@@ -132,7 +107,6 @@ open class SheetView: NSObject, UIPickerViewDelegate, UIPickerViewDataSource {
         }
         self.show(mainView, duration: duration, completion: completion)
     }
-    
     public func showDatePicker(_ date: Date, inView mainView: UIView, minimumDate: Date? = nil, maximumDate: Date? = nil, duration: TimeInterval = 0.3, canBack: Bool = false, completion: ((Bool) -> Void)? = nil, locale: Locale? = nil) {
         _pickerView.alpha = 0
         _datePicker.alpha = 1.0
@@ -142,26 +116,17 @@ open class SheetView: NSObject, UIPickerViewDelegate, UIPickerViewDataSource {
         _backBtn.setTitle(SheetView.backBtnTitle, for: UIControl.State())
         _backBtn.isHidden = !canBack
         _datePicker.date = date
-        _datePicker.setValue(SheetView.textColor, forKeyPath: "textColor")
-        if #available(iOS 13.0, *) {
-            _datePicker.setValue(false, forKey: "highlightsToday")
-        }
         self._datePicker.locale = locale
         _datePicker.minimumDate = minimumDate
         _datePicker.maximumDate = maximumDate
         self.show(mainView, duration: duration, completion: completion)
     }
-    
-    
     public func show(_ mainView: UIView, duration: TimeInterval = 0.3, completion: ((Bool) -> Void)? = nil) {
-        
         if let _ = _view.superview {
             return
         }
-        
         DispatchQueue.main.async(execute: {
             mainView.addSubview(self._view)
-            
             UIView.animate(withDuration: duration, animations: {
                 var frame = self._customView.frame
                 frame.origin.y = self._view.frame.height - frame.size.height
@@ -169,12 +134,10 @@ open class SheetView: NSObject, UIPickerViewDelegate, UIPickerViewDataSource {
             }, completion: completion)
         })
     }
-    
     public func dismiss(isBack back: Bool = false, duration: TimeInterval = 0.3, completion: (() -> Void)? = nil) {
         if (_view.superview == nil){
             return
         }
-        
         DispatchQueue.main.async(execute: {
             for i in 0 ..< self.pickerSource.count {
                 if back {
@@ -183,7 +146,6 @@ open class SheetView: NSObject, UIPickerViewDelegate, UIPickerViewDataSource {
                     self.delegate?.dismissWithPick?(row: self._pickerView.selectedRow(inComponent: i), forComponent: i)
                 }
             }
-            
             if self._datePicker.alpha != 0 {
                 if back {
                     self.delegate?.backWithPickDate?(self._datePicker.date)
@@ -191,11 +153,8 @@ open class SheetView: NSObject, UIPickerViewDelegate, UIPickerViewDataSource {
                     self.delegate?.dismissWithPickDate?(self._datePicker.date)
                 }
             }
-            
             self.delegate = nil
-            
             self.pickerSource = Array<[Any]>()
-            
             UIView.animate(withDuration: duration, animations: {
                 var frame = self._customView.frame
                 frame.origin.y = self._view.frame.height
@@ -206,24 +165,19 @@ open class SheetView: NSObject, UIPickerViewDelegate, UIPickerViewDataSource {
             })
         })
     }
-    
     @objc public class func backgroundTapped() {
         instance.dismiss(isBack: true)
     }
-    
     //MARK: UIPickerViewDelegate
-    
     public func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return pickerSource.count
     }
-    
     public func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         if component < pickerSource.count {
             return pickerSource[component].count
         }
         return 0
     }
-    
     public func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
         var label = view as? UILabel
         if (label == nil) {
@@ -232,35 +186,27 @@ open class SheetView: NSObject, UIPickerViewDelegate, UIPickerViewDataSource {
         }
         label?.font = SheetView.font
         label?.textColor = SheetView.textColor
-        
         if pickerSource.count > component && pickerSource[component].count > row {
             let dataSource = pickerSource[component][row]
             if let text = dataSource as? String {
                 label?.text = text
             }
         }
-        
         return label!
     }
-    
     public func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         delegate?.didPickItem?(row: row, inComponent: component)
     }
-    
     @objc public func dateChanged() {
         delegate?.didPickDate?(_datePicker.date)
     }
     @objc public func selected() {
         dismiss()
     }
-    
     @objc public func back() {
         dismiss(isBack: true)
     }
-    
     open func getCalendar() -> Calendar {
         return Calendar(identifier: Calendar.Identifier.gregorian)
     }
 }
-
-
