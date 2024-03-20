9f6f73e2bdce0d3bfec01264cb780cc6ac72d5f8
木村太一朗 <kimura@tanosys.com>
2024-03-21 08:18:32 +0900

    remove socketio

--- SwiftJsonUI.podspec
+++ SwiftJsonUI.podspec
@@ -40,6 +40,4 @@ TODO: Add long description of the pod here.
   # s.public_header_files = 'Pod/Classes/**/*.h'
   # s.frameworks = 'UIKit', 'MapKit'
   s.platform = :ios, '13.0'
-  s.dependency 'Socket.IO-Client-Swift', '~> 16.1.0'
-  s.dependency 'Starscream', '~> 4.0.8'
 end
--- SwiftJsonUI/Classes/HotLoader.swift
+++ SwiftJsonUI/Classes/HotLoader.swift
@@ -7,9 +7,7 @@
 import UIKit
 
 #if DEBUG
-import SocketIO
-let socketManager = SocketManager(socketURL: URL(string: "http://\((Bundle.main.object(forInfoDictionaryKey: "CurrentIp") as? String) ?? ""):\((Bundle.main.object(forInfoDictionaryKey: "HotLoader Port") as? String) ?? "8080")")!)
-public class HotLoader {
+public class HotLoader: NSObject, URLSessionWebSocketDelegate {
     
     private static var Instance = HotLoader()
     
@@ -30,41 +28,74 @@ public class HotLoader {
         }
     }
     
-    private weak var _socket: SocketIOClient?
+    private var _webSocketTask: URLSessionWebSocketTask?
+    
+    private var _delegateQueue = DispatchQueue(label: "swiftjsonui.hotloader")
     
     private func connectToSocket() {
-        socketManager.handleQueue.async(execute: {
-            let socket = socketManager.defaultSocket
-            socket.on(clientEvent: .connect) {data, ack in
-                Logger.debug("socket connected")
-            }
-            socket.on("layoutChanged") {data, ack in
-                Logger.debug("\(data[0])")
-                Logger.debug("\(data[1])")
-                Logger.debug("\(data[2])")
-                if let strs = data as? [String], strs.count >= 3 {
-                    self.downloadLayout(layoutPath: strs[0], dirName: strs[1], fileName: strs[2])
+        let urlSession = URLSession(configuration: .default, delegate: self, delegateQueue: OperationQueue())
+        let url = URL(string: "ws://\((Bundle.main.object(forInfoDictionaryKey: "CurrentIp") as? String) ?? ""):\((Bundle.main.object(forInfoDictionaryKey: "HotLoader Port") as? String) ?? "8080")")!
+        _webSocketTask = urlSession.webSocketTask(with: url)
+        _webSocketTask?.resume()
+        receiveMessage()
+    }
+    
+    private func receiveMessage() {
+        _webSocketTask?.receive { [weak self] result in
+            switch result {
+              case .success(let message):
+                do {
+                    switch message {
+                    case .string(let text):
+                        if let data = text.data(using: .utf8) {
+                            
+                            if let strs = try JSONSerialization.jsonObject(with: data, options: []) as? [String],  strs.count >= 3 {
+                                Logger.debug("SwiftJSONUIHotloader \(strs[0])")
+                                Logger.debug("SwiftJSONUIHotloader \(strs[1])")
+                                Logger.debug("SwiftJSONUIHotloader \(strs[2])")
+                                self?.downloadLayout(layoutPath: strs[0], dirName: strs[1], fileName: strs[2])
+                            }
+                            
+                        }
+                    case .data(let data):
+                        print("SwiftJSONUIHotloader Received! binary: \(data)")
+                        if let strs = try JSONSerialization.jsonObject(with: data, options: []) as? [String],  strs.count >= 3 {
+                            Logger.debug("SwiftJSONUIHotloader \(strs[0])")
+                            Logger.debug("SwiftJSONUIHotloader \(strs[1])")
+                            Logger.debug("SwiftJSONUIHotloader \(strs[2])")
+                            self?.downloadLayout(layoutPath: strs[0], dirName: strs[1], fileName: strs[2])
+                        }
+                    @unknown default:
+                        fatalError()
+                    }
+                } catch {
+                    Logger.debug("SwiftJSONUIHotloader Parse Json Error: \(error.localizedDescription)")
                 }
+                self?.receiveMessage()  // <- 継続して受信するために再帰的に呼び出す
+              case .failure(let error):
+                Logger.debug("SwiftJSONUIHotloader Failed! error: \(error)")
             }
-            
-            socket.once(clientEvent: .disconnect, callback: {data, ack in
-                let notification = NSNotification.Name("socketDidDisConnected")
-                NotificationCenter.default.post(name: notification, object: nil)
-            })
-            socket.connect()
-            self._socket = socket
-        })
+        }
     }
     
     private func disconnectFromSocket() {
-        socketManager.handleQueue.async(execute: {
-            self._socket?.disconnect()
-        })
+        Logger.debug("SwiftJSONUIHotloader socket disconnect")
+        _webSocketTask?.cancel()
+    }
+    
+    public func urlSession(_ session: URLSession, webSocketTask: URLSessionWebSocketTask, didOpenWithProtocol protocol: String?) {
+        Logger.debug("SwiftJSONUIHotloader socket connected")
+    }
+    
+    
+    public func urlSession(_ session: URLSession, webSocketTask: URLSessionWebSocketTask, didCloseWith closeCode: URLSessionWebSocketTask.CloseCode, reason: Data?) {
+        let notification = NSNotification.Name("SwiftJSONUIHotloader socketDidDisConnected")
+        NotificationCenter.default.post(name: notification, object: nil)
     }
     
     private func downloadLayout(layoutPath: String, dirName: String, fileName: String) {
         if let url = URL(string: "http://\((Bundle.main.object(forInfoDictionaryKey: "CurrentIp") as? String) ?? ""):3000/\(layoutPath)?file_path=\(fileName)&dir_name=\(dirName)&\(additionalRequestParameter)") {
-            Logger.debug("\(url.absoluteString)")
+            Logger.debug("SwiftJSONUIHotloader \(url.absoluteString)")
             let downloader = Downloader(url: url)
             downloader.completionHandler = { data, exist in
                 let fm = FileManager.default
@@ -86,13 +117,13 @@ public class HotLoader {
                         try fm.removeItem(atPath: toPath)
                     }
                     fm.createFile(atPath: toPath, contents:data, attributes:nil)
-                    Logger.debug("Layout Updated")
+                    Logger.debug("SwiftJSONUIHotloader Layout Updated")
                     DispatchQueue.main.async(execute: {
                         let notification = NSNotification.Name("layoutFileDidChanged")
                         NotificationCenter.default.post(name: notification, object: nil)
                     })
                 } catch let error {
-                    Logger.debug("Error: \(error)")
+                    Logger.debug("SwiftJSONUIHotloader Error: \(error)")
                 }
             }
             downloader.start(true)
