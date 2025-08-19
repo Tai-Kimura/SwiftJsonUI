6d2b3f41f4d4a46cc04df1e334f00b97216e343d
木村太一朗 <kimura@tanosys.com>
2025-08-20 08:30:20 +0900

    Add StateAwareButton support to Static ButtonConverter
    - Generate StateAwareButtonView for buttons with state attributes
    - Support tapBackground, hilightColor, disabledFontColor, disabledBackground
    - Provides visual feedback for pressed and disabled states in Static mode

--- sjui_tools/lib/swiftui/views/button_converter.rb
+++ sjui_tools/lib/swiftui/views/button_converter.rb
@@ -11,6 +11,13 @@ module SjuiTools
       class ButtonConverter < BaseViewConverter
         include SjuiTools::SwiftUI::Helpers::FontHelper
         def convert
+          # Check if we need to use StateAwareButton
+          if @component['tapBackground'] || @component['hilightColor'] || 
+             @component['disabledFontColor'] || @component['disabledBackground']
+            return convert_state_aware_button
+          end
+          
+          # Original convert method continues below
           text = @component['text'] || "Button"
           # onclickを使用（SwiftJsonUIの属性）
           action = @component['onclick']
@@ -113,6 +120,106 @@ module SjuiTools
         end
         
         private
+        
+        def convert_state_aware_button
+          text = @component['text'] || "Button"
+          action = @component['onclick']
+          
+          # Use StateAwareButtonView for state-dependent styling
+          add_line "StateAwareButtonView("
+          indent do
+            # Text
+            escaped_text = text.gsub('"', '\\"')
+            add_line "text: \"#{escaped_text}\","
+            
+            # Action
+            if action
+              if action.include?(':')
+                method_name = action.gsub(':', '')
+                add_line "action: { viewModel.#{method_name}(self) },"
+              else
+                add_line "action: { viewModel.#{action}() },"
+              end
+            else
+              add_line "action: { },"
+            end
+            
+            # Font properties
+            if @component['fontSize']
+              add_line "fontSize: #{@component['fontSize'].to_i},"
+            end
+            if @component['fontWeight']
+              add_line "fontWeight: \"#{@component['fontWeight']}\","
+            end
+            
+            # Color properties
+            if @component['fontColor']
+              add_line "fontColor: #{hex_to_swiftui_color(@component['fontColor'])},"
+            end
+            if @component['background']
+              add_line "backgroundColor: #{hex_to_swiftui_color(@component['background'])},"
+            end
+            
+            # State-dependent colors
+            if @component['tapBackground']
+              add_line "tapBackground: #{hex_to_swiftui_color(@component['tapBackground'])},"
+            end
+            if @component['hilightColor']
+              add_line "hilightColor: #{hex_to_swiftui_color(@component['hilightColor'])},"
+            end
+            if @component['disabledFontColor']
+              add_line "disabledFontColor: #{hex_to_swiftui_color(@component['disabledFontColor'])},"
+            end
+            if @component['disabledBackground']
+              add_line "disabledBackground: #{hex_to_swiftui_color(@component['disabledBackground'])},"
+            end
+            
+            # Other properties
+            if @component['cornerRadius']
+              add_line "cornerRadius: #{@component['cornerRadius'].to_i},"
+            end
+            
+            # Padding
+            if @component['padding']
+              padding = @component['padding']
+              if padding.is_a?(Array)
+                case padding.length
+                when 1
+                  add_line "padding: EdgeInsets(top: #{padding[0]}, leading: #{padding[0]}, bottom: #{padding[0]}, trailing: #{padding[0]}),"
+                when 2
+                  add_line "padding: EdgeInsets(top: #{padding[0]}, leading: #{padding[1]}, bottom: #{padding[0]}, trailing: #{padding[1]}),"
+                when 4
+                  add_line "padding: EdgeInsets(top: #{padding[0]}, leading: #{padding[3]}, bottom: #{padding[2]}, trailing: #{padding[1]}),"
+                end
+              else
+                add_line "padding: EdgeInsets(top: #{padding}, leading: #{padding}, bottom: #{padding}, trailing: #{padding}),"
+              end
+            end
+            
+            # Enabled state
+            if @component['enabled'] != nil
+              enabled_value = @component['enabled']
+              if enabled_value.is_a?(String) && enabled_value.start_with?('@{') && enabled_value.end_with?('}')
+                property_name = enabled_value[2...-1]
+                add_line "isEnabled: viewModel.data.#{property_name}"
+              elsif enabled_value == false
+                add_line "isEnabled: false"
+              elsif enabled_value == true
+                add_line "isEnabled: true"
+              end
+            else
+              add_line "isEnabled: true"
+            end
+          end
+          add_line ")"
+          
+          # Apply frame constraints and margins
+          apply_frame_constraints
+          apply_frame_size
+          apply_margins
+          
+          generated_code
+        end
         
         def apply_padding_to_text
           # Apply padding to the Text inside the button (not to the button itself)
